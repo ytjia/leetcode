e7e95a29d64bbaa534ca18e902fe96a59d378ba7
Y. Jia <ytjia.zju@gmail.com>
2017-09-27 14:28:34 +0800

    daily update of 20170927

---
+++ algorithms/java/src/main/java/leetcode/LongestAbsoluteFilePath.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * @author ytjia created on 2017-09-25 17:53
+ */
+public class LongestAbsoluteFilePath {
+
+  /**
+   * https://leetcode.com/problems/longest-absolute-file-path/description/
+   */
+  class Solution {
+
+    public int lengthLongestPath(String input) {
+      if (null == input || input.length() == 0) {
+        return 0;
+      }
+
+      int maxLength = 0;
+      String[] dirs = input.split("\n");
+
+      for (String dir: dirs) {
+        String[]
+      }
+
+    }
+  }
+
+}
--- algorithms/java/src/main/java/leetcode/SortColors.java
+++ algorithms/java/src/main/java/leetcode/SortColors.java
@@ -17,7 +17,29 @@ public class SortColors {
   class Solution {
 
     public void sortColors(int[] nums) {
+      if (null == nums || nums.length <= 1) {
+        return;
+      }
 
+      int lens = nums.length;
+      int zero = 0;
+      int second = lens - 1;
+
+      for (int i = 0; i <= second; i++) {
+        while (nums[i] == 2 && i < second) {
+          swap(nums, i, second--);
+        }
+        while (nums[i] == 0 && i > zero) {
+          swap(nums, i, zero++);
+        }
+      }
+
+    }
+
+    private void swap(int[] nums, int i, int j) {
+      int tmp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = tmp;
     }
   }
 
